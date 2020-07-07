f1aead31ff75bf36c5bb9264e43d765e1c35281c
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-07-07 12:44:55 +0600

    users message block, message isReaded/unreaded  icon

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import distanseInWordsToNow from 'date-fns/distance_in_words_to_now';
 import ruLocale from 'date-fns/locale/ru';
+import classNames from 'classnames';
+import readedSvg from 'assets/svg/readed.svg';
+import unreadedSvg from 'assets/svg/unreaded.svg';
 import './message.scss';
 
-const Message = ({ avatar, user, text, date }) => {
+const Message = ({ avatar, user, text, date, isMe, isReaded }) => {
   return (
-    <div className='message'>
+    <div className={classNames('message', { 'message-isMe': isMe })}>
       <div className='message__avatar'>
         <img src={avatar} alt={`Avatar ${user.fullname}`} />
       </div>
       <div className='message__content'>
+        {isMe && isReaded && <img className={'message__readed'} src={readedSvg} alt='is Readed' />}
+				{isMe && !isReaded && <img className={'message__unreaded'} src={unreadedSvg} alt='unread' />}
         <div className='message__bubble'>
           <p className='message__text'>{text}</p>
         </div>
@@ -30,7 +34,7 @@ Message.defaultProps = {
 Message.propTypes = {
   avatar: PropTypes.string,
   text: PropTypes.string,
-  // date: PropTypes.string,
+  date: PropTypes.string,
   userr: PropTypes.object,
 };
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,28 +5,48 @@ import './home.scss';
 
 function Home() {
   return (
-    <div className="home">
-      <Message 
-			avatar="https://lh3.googleusercontent.com/S1Ms7L33svJK-4HX-HNQPTwL-C7PMGZVqMGHEJb56RMZ3_o4yguJcg-z2FruYUoG-7Mh=s85"
-			text="Мы тут недавно войска Ариовиста разбили, чуваки хотели закрепиться на галльских землях, лол 🌝"
-			date="Tue Jul 07 2020 07:29:27"
-			isMe={false}
-			/>
-			 <Message 
-			avatar="https://lh3.googleusercontent.com/S1Ms7L33svJK-4HX-HNQPTwL-C7PMGZVqMGHEJb56RMZ3_o4yguJcg-z2FruYUoG-7Mh=s85"
-			text="Мы тут недавно войска Ариовиста разбили, чуваки хотели закрепиться на галльских землях, лол 🌝"
-			date="Tue Jul 07 2020 07:29:27"
-			/>
-			 <Message 
-			avatar="https://lh3.googleusercontent.com/S1Ms7L33svJK-4HX-HNQPTwL-C7PMGZVqMGHEJb56RMZ3_o4yguJcg-z2FruYUoG-7Mh=s85"
-			text="Мы тут недавно войска Ариовиста разбили, чуваки хотели закрепиться на галльских землях, лол 🌝"
-			date="Tue Jul 07 2020 07:29:27"
-			/>
-			 <Message 
-			avatar="https://lh3.googleusercontent.com/S1Ms7L33svJK-4HX-HNQPTwL-C7PMGZVqMGHEJb56RMZ3_o4yguJcg-z2FruYUoG-7Mh=s85"
-			text="Мы тут недавно войска Ариовиста разбили, чуваки хотели закрепиться на галльских землях, лол 🌝"
-			date="Tue Jul 07 2020 07:29:27"
-			/>
+    <div className='home'>
+      <Message
+        avatar='https://lh3.googleusercontent.com/S1Ms7L33svJK-4HX-HNQPTwL-C7PMGZVqMGHEJb56RMZ3_o4yguJcg-z2FruYUoG-7Mh=s85'
+        text='Салам, Брут! Чё, как, уничтожил флот галлов? 🖐🏻'
+        date='Tue Jul 07 2020 07:29:27'
+      />
+			 <Message
+        avatar='https://lh3.googleusercontent.com/S1Ms7L33svJK-4HX-HNQPTwL-C7PMGZVqMGHEJb56RMZ3_o4yguJcg-z2FruYUoG-7Mh=s85'
+        text='Мы тут недавно войска Ариовиста разбили, чуваки хотели закрепиться на галльских землях, лол 🌝'
+        date='Tue Jul 07 2020 07:29:27'
+      />
+      <Message
+        avatar='https://lh3.googleusercontent.com/YwcsCTzF--Hb_15uHBb8eXS4YbN_IBTcM88anuVp6R8XI14V3FT4SqrvgYjI_6QKUXwn=s85'
+        text='Всё норм! Флот построили на Лауре. Галлов добили после вашего нападения. Спс! 🔥'
+        date='Tue Jul 07 2020 07:29:27'
+				isMe={true}
+				isReaded={true}
+      />
+      <Message
+        avatar='https://lh3.googleusercontent.com/S1Ms7L33svJK-4HX-HNQPTwL-C7PMGZVqMGHEJb56RMZ3_o4yguJcg-z2FruYUoG-7Mh=s85'
+        text='Да нзч! Ок, держи в курсе.'
+        date='Tue Jul 07 2020 07:29:27'
+      />
+      <Message
+        avatar='https://lh3.googleusercontent.com/YwcsCTzF--Hb_15uHBb8eXS4YbN_IBTcM88anuVp6R8XI14V3FT4SqrvgYjI_6QKUXwn=s85'
+        text='Ave Caesar! Morituri te salutant! ☝️'
+        date='Tue Jul 07 2020 07:29:27'
+				isMe={true}
+				isReaded={true}
+      />
+      <Message
+        avatar='https://lh3.googleusercontent.com/S1Ms7L33svJK-4HX-HNQPTwL-C7PMGZVqMGHEJb56RMZ3_o4yguJcg-z2FruYUoG-7Mh=s85'
+        text='Кек. Ты то меня никогда не предашь 😌'
+        date='Tue Jul 07 2020 07:29:27'
+      />
+			 <Message
+        avatar='https://lh3.googleusercontent.com/YwcsCTzF--Hb_15uHBb8eXS4YbN_IBTcM88anuVp6R8XI14V3FT4SqrvgYjI_6QKUXwn=s85'
+        text='Всё норм! Флот построили на Лауре. Галлов добили после вашего нападения. Спс! 🔥'
+        date='Tue Jul 07 2020 07:29:27'
+				isMe={true}
+				isReaded={false}
+      />
     </div>
   );
 }
